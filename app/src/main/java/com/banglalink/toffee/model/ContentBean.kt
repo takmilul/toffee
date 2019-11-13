6a2dc2b67df16fdd8d9d7237cc3f819a403d0c87
rokibhasan <rokib.buet06@gmail.com>
2019-11-13 18:47:00 +0600

    Alteranate tag added because of handling shareable content response

--- app/src/main/java/com/banglalink/toffee/model/ContentBean.kt
+++ app/src/main/java/com/banglalink/toffee/model/ContentBean.kt
@@ -1,11 +1,13 @@
 package com.banglalink.toffee.model
 
 import com.banglalink.toffee.ui.player.ChannelInfo
+import com.google.gson.annotations.SerializedName
 
 data class ContentBean(
+    @SerializedName(value = "channels", alternate = ["channelInfo"])
     val channels: List<ChannelInfo>?,
     val count: Int,
     val totalCount: Int,
     val balance: Int,
-    val systemTime:String
+    val systemTime: String
 )
