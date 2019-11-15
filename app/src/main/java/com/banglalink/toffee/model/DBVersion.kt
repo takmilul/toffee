6afe7320c46ca0bb2f9297454507c3e3add85ea3
rokibhasan <rokib.buet06@gmail.com>
2019-11-15 18:59:18 +0600

    DB version json annotation changed

--- app/src/main/java/com/banglalink/toffee/model/DBVersion.kt
+++ app/src/main/java/com/banglalink/toffee/model/DBVersion.kt
@@ -1,9 +1,18 @@
 package com.banglalink.toffee.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DBVersion(
+    @SerializedName(value = "channel_db_version",alternate = ["chanelDbVersion"])
     val chanelDbVersion: Int,
+    @SerializedName(value = "vod_db_version",alternate = ["vodDbVersion"])
     val vodDbVersion: Int,
+    @SerializedName("notification_db_version",alternate = ["notificationDbVersion"])
     val notificationDbVersion: Int,
+    @SerializedName(value = "catchup_db_version",alternate = ["catchupDbVersion"])
     val catchupDbVersion: Int,
-    val packageDbVersion: Int
+    @SerializedName(value = "package_db_version",alternate = ["packageDbVersion"])
+    val packageDbVersion: Int,
+    @SerializedName("category_db_version",alternate = ["categoryDbVersion"])
+    val categoryDbVersion:Int
 )
