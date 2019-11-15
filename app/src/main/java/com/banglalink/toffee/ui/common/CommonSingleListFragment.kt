d2b5bb0dfbf4a950bbe73f972bce26db5a1dd65a
rokibhasan <rokib.buet06@gmail.com>
2019-11-15 18:55:36 +0600

    Favorite and Catchup now inherit from CommonList Adapter

---
+++ app/src/main/java/com/banglalink/toffee/ui/common/CommonSingleListFragment.kt
@@ -0,0 +1,144 @@
+package com.banglalink.toffee.ui.common
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.banglalink.toffee.R
+import com.banglalink.toffee.databinding.FragmentCatchupBinding
+import com.banglalink.toffee.extension.showToast
+import com.banglalink.toffee.listeners.EndlessRecyclerViewScrollListener
+import com.banglalink.toffee.model.Resource
+import com.banglalink.toffee.ui.home.HomeViewModel
+import com.banglalink.toffee.ui.widget.MyPopupWindow
+import com.banglalink.toffee.ui.home.OptionCallBack
+import com.banglalink.toffee.model.ChannelInfo
+
+abstract class CommonSingleListFragment:Fragment(), OptionCallBack {
+    val homeViewModel by lazy {
+        ViewModelProviders.of(activity!!).get(HomeViewModel::class.java)
+    }
+
+    protected val baseViewModel by lazy {
+        ViewModelProviders.of(this).get(BaseViewModel::class.java)
+    }
+
+    lateinit var mAdapter: CommonChannelAdapter
+    lateinit var scrollListener: EndlessRecyclerViewScrollListener
+    private var title: String? = null
+
+    lateinit var binding: FragmentCatchupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loadItems(0)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_catchup,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeFavoriteLiveData()
+
+        title = arguments?.getString("title")
+        activity?.title = title
+        mAdapter= CommonChannelAdapter(this) {
+            homeViewModel.fragmentDetailsMutableLiveData.postValue(it)
+        }
+        val linearLayoutManager = LinearLayoutManager(context)
+        binding.listview.layoutManager = linearLayoutManager
+        binding.listview.adapter = mAdapter
+        scrollListener = object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                binding.progressBar.visibility =View.GONE
+                loadItems(mAdapter.getOffset())
+            }
+        }
+        // Adds the scroll listener to RecyclerView
+        binding.listview.addOnScrollListener(scrollListener)
+        binding.progress.visibility = View.VISIBLE
+    }
+
+    abstract fun loadItems(offset:Int)
+
+    protected open fun observeFavoriteLiveData(){
+        baseViewModel.favoriteLiveData.observe(viewLifecycleOwner, Observer {
+            when(it){
+                is Resource.Success->{
+                    val channelInfo = it.data
+                    when(channelInfo.favorite){
+                        "0"->{
+                            context?.showToast("Content successfully removed from favorite list")
+                            onFavoriteItemRemoved(channelInfo)
+                        }
+                        "1"->context?.showToast("Content successfully added to favorite list")
+                    }
+                }
+                is Resource.Failure->{
+                    context?.showToast(it.error.msg)
+                }
+            }
+        })
+    }
+
+
+    override fun onOptionClicked(anchor: View, channelInfo: ChannelInfo) {
+        val popupMenu = MyPopupWindow(context!!, anchor)
+        popupMenu.inflate(R.menu.menu_catchup_item)
+
+        if (channelInfo.favorite == null || channelInfo.favorite == "0") {
+            popupMenu.menu.getItem(0).title = "Add to Favorites"
+        } else {
+            popupMenu.menu.getItem(0).title = "Remove from Favorites"
+        }
+        popupMenu.setOnMenuItemClickListener{
+            when(it?.itemId){
+                R.id.menu_share->{
+                    val sharingIntent = Intent(Intent.ACTION_SEND)
+                    sharingIntent.type = "text/plain"
+                    sharingIntent.putExtra(
+                        Intent.EXTRA_TEXT,
+                        channelInfo.video_share_url
+                    )
+                    activity?.startActivity(Intent.createChooser(sharingIntent, "Share via"))
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.menu_fav->{
+                    baseViewModel.updateFavorite(channelInfo)
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.menu_not_interested->{
+                    mAdapter?.remove(channelInfo)
+                    return@setOnMenuItemClickListener true
+                }
+                else->{
+                    return@setOnMenuItemClickListener false
+                }
+            }
+        }
+        popupMenu.show()
+    }
+
+    override fun viewAllVideoClick() {
+        homeViewModel.viewAllVideoLiveData.postValue(true)
+    }
+
+    abstract fun onFavoriteItemRemoved(channelInfo: ChannelInfo)
+
+    fun hideProgress(){
+        binding.progressBar.visibility = View.GONE
+        binding.progress.visibility = View.GONE
+    }
+}
--- app/src/main/java/com/banglalink/toffee/ui/home/CatchupFragment.kt
+++ app/src/main/java/com/banglalink/toffee/ui/home/CatchupFragment.kt
@@ -2,97 +2,54 @@ package com.banglalink.toffee.ui.home
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.banglalink.toffee.R
-import com.banglalink.toffee.databinding.FragmentCatchupBinding
-import com.banglalink.toffee.listeners.EndlessRecyclerViewScrollListener
 import com.banglalink.toffee.model.Resource
-import com.banglalink.toffee.ui.common.CommonChannelAdapter
-import com.banglalink.toffee.ui.common.HomeBaseFragment
 import com.banglalink.toffee.model.ChannelInfo
-import com.banglalink.toffee.ui.widget.VelBoxProgressDialog
+import com.banglalink.toffee.ui.common.CommonSingleListFragment
 
-class CatchupFragment:HomeBaseFragment(){
-    override fun removeItemNotInterestedItem(channelInfo: ChannelInfo) {
-        mAdapter.remove(channelInfo)
+class CatchupFragment : CommonSingleListFragment() {
+    override fun onFavoriteItemRemoved(channelInfo: ChannelInfo) {
+        //do nothing
     }
 
-
-    lateinit var mAdapter: CommonChannelAdapter
-    lateinit var scrollListener: EndlessRecyclerViewScrollListener
-    private var title: String? = null
-    private var loading: Boolean = false
-
-    lateinit var binding:FragmentCatchupBinding
 
     private val viewModel by lazy {
         ViewModelProviders.of(this).get(CatchupViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         viewModel.parseBundle(arguments!!)
+        super.onCreate(savedInstanceState)
 
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_catchup,container,false)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        title = arguments?.getString("title")
+        val title = arguments?.getString("title")
         activity?.title = title
-        val progressDialog = VelBoxProgressDialog(context!!)
-        mAdapter= CommonChannelAdapter(this) {
-            homeViewModel.fragmentDetailsMutableLiveData.postValue(it)
-        }
-        val linearLayoutManager = LinearLayoutManager(context)
-        binding.listview.layoutManager = linearLayoutManager
-        binding.listview.adapter = mAdapter
-        scrollListener = object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
-            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
-               loadItems(mAdapter.getOffset())
-            }
-        }
-        // Adds the scroll listener to RecyclerView
-        binding.listview.addOnScrollListener(scrollListener)
+
         viewModel.contentLiveData.observe(viewLifecycleOwner, Observer {
-           when(it){
-               is Resource.Success->{
-                   if ( progressDialog.isShowing) progressDialog.dismiss()
-                   loading = false
-                   binding.progressBar.visibility = View.GONE
-                   mAdapter.addAll(it.data)
-               }
-               is Resource.Failure->{
-                   if (progressDialog.isShowing) progressDialog.dismiss()
-                   binding.progressBar.visibility = View.GONE
-                   Log.e("TAG","Failure")
-               }
-           }
+            when (it) {
+                is Resource.Success -> {
+                    hideProgress()
+                    mAdapter.addAll(it.data)
+                }
+                is Resource.Failure -> {
+                    hideProgress()
+                    Log.e("TAG", "Failure")
+                }
+            }
         })
-        progressDialog.show()
-        loadItems(mAdapter.itemCount)
     }
 
-    private fun loadItems(offset:Int){
+    override fun loadItems(offset: Int) {
         binding.progressBar.visibility = View.VISIBLE
-        loading = true
         viewModel.getContent(offset)
     }
-    companion object{
+
+    companion object {
         fun createInstance(
             categoryID: Int,
             subCategoryID: Int,
@@ -124,11 +81,10 @@ class CatchupFragment:HomeBaseFragment(){
     ) {
         activity?.title = title
         binding.progressBar.visibility = View.VISIBLE
-        loading = true
         mAdapter.removeAll()
         mAdapter.clearOffset()
         mAdapter.notifyDataSetChanged()
-        viewModel.updateInfo(category,categoryId,subCategory,subCategoryID,type)
+        viewModel.updateInfo(category, categoryId, subCategory, subCategoryID, type)
     }
 
 }
