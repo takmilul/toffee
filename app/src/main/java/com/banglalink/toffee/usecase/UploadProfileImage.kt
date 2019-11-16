2e3ba45354e048afdc23491b1c3a20a37d8e7466
rokibhasan <rokib.buet06@gmail.com>
2019-11-16 13:35:08 +0600

    Upload profile image encode now moved to coroutine

--- app/src/main/java/com/banglalink/toffee/usecase/UploadProfileImage.kt
+++ app/src/main/java/com/banglalink/toffee/usecase/UploadProfileImage.kt
@@ -1,24 +1,43 @@
 package com.banglalink.toffee.usecase
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.util.Base64
 import com.banglalink.toffee.data.network.request.UploadProfileImageRequest
 import com.banglalink.toffee.data.network.retrofit.ToffeeApi
 import com.banglalink.toffee.data.network.util.tryIO
 import com.banglalink.toffee.data.storage.Preference
+import com.banglalink.toffee.util.decodeSampledBitmap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
 
 class UploadProfileImage(private val preference: Preference, private val toffeeApi: ToffeeApi) {
 
     suspend fun execute(
-        profilePhoto: String
+        photoUri: Uri,context:Context
     ): Boolean {
-        tryIO {
-            toffeeApi.uploadPhoto(
-                UploadProfileImageRequest(
-                    profilePhoto,
-                    preference.customerId,
-                    preference.password
+        return withContext(Dispatchers.Default){
+            val imageBitmap = decodeSampledBitmap(context, photoUri)
+            val bao = ByteArrayOutputStream()
+            imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 50, bao)
+            val imageData = bao.toByteArray()
+            val imageString = Base64.encodeToString(imageData, Base64.DEFAULT)
+
+            tryIO {
+                toffeeApi.uploadPhoto(
+                    UploadProfileImageRequest(
+                        imageString,
+                        preference.customerId,
+                        preference.password
+                    )
                 )
-            )
+            }
+            true
         }
-        return true
+
     }
+
+
 }
