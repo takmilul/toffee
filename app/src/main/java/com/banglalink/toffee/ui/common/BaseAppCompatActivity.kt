2c023e135954a1402bbd06d3da9116d39da08207
rokibhasan <rokib.buet06@gmail.com>
2019-11-26 17:43:37 +0600

    Uncaught exception handler is associated with BaseAppCompatActivity. Whenever CustomerNotFoundException is thrown across the app, this activity will redirect it to the SignInByPhoneActivity

--- app/src/main/java/com/banglalink/toffee/ui/common/BaseAppCompatActivity.kt
+++ app/src/main/java/com/banglalink/toffee/ui/common/BaseAppCompatActivity.kt
@@ -1,19 +1,33 @@
 package com.banglalink.toffee.ui.common
 
-import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.banglalink.toffee.data.storage.Preference
+import com.banglalink.toffee.exception.CustomerNotFoundException
+import com.banglalink.toffee.extension.launchActivity
+import com.banglalink.toffee.ui.login.SigninByPhoneActivity
+import kotlin.system.exitProcess
 
 
-open class BaseAppCompatActivity : AppCompatActivity() {
+abstract class BaseAppCompatActivity : AppCompatActivity(), Thread.UncaughtExceptionHandler {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.setDefaultUncaughtExceptionHandler(this)
+    }
 
-    fun showAlertDialog(message: String, cancellable: Boolean) {
-
-        AlertDialog.Builder(this)
-            .setMessage(message)
-            .setCancelable(cancellable)
-            .setPositiveButton("Ok") { dialog, which -> dialog.dismiss()}
-            .show()
-        return
+    //we are catching the CustomerNotFoundException across the app so that we can goto login screen
+    override fun uncaughtException(p0: Thread?, p1: Throwable?) {
+        if (p1 is CustomerNotFoundException) {
+            Preference.getInstance().clear()
+            launchActivity<SigninByPhoneActivity>{
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            finish()
+        } else {
+            p1?.printStackTrace()
+           exitProcess(1)
+        }
     }
 }
