c83516de7dab84ddc1e019d7cb4c3d3e4a468921
rokibhasan <rokib.buet06@gmail.com>
2019-11-27 16:02:05 +0600

    disableSSLCertificateVerify method removed

--- app/src/main/java/com/banglalink/toffee/util/Utils.java
+++ app/src/main/java/com/banglalink/toffee/util/Utils.java
@@ -281,39 +281,5 @@ public class Utils {
         return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 
-    public static void disableSSLCertificateVerify() {
-        TrustManager[] trustAllCerts = new TrustManager[] {
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() {
-                        X509Certificate[] myTrustedAnchors = new X509Certificate[0];
-                        return myTrustedAnchors;
-                    }
-
-                    @Override
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-
-                    @Override
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-                }
-        };
-
-        try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-
-            sc.init(null, trustAllCerts, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
