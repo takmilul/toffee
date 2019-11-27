8d2923d9af2de4894f2141b8a15492562fecc298
shantanu <shantanu.csedu@gmail.com>
2019-11-27 22:19:22 +0600

    fix content doesn't go under status bar when full screen

--- app/src/main/java/com/banglalink/toffee/util/Utils.java
+++ app/src/main/java/com/banglalink/toffee/util/Utils.java
@@ -207,7 +207,15 @@ public class Utils {
     }
 
     public static void setFullScreen(Activity activity, boolean visible) {
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
 
         if (!visible) {
             activity.getWindow().getDecorView().setSystemUiVisibility(0);
