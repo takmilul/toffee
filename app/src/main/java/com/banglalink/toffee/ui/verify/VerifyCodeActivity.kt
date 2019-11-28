38a60e451907bd55e588fa43949d516866932e1e
rokibhasan <rokib.buet06@gmail.com>
2019-11-28 17:05:57 +0600

    Unsafe lazy introduced and Data binding added

--- app/src/main/java/com/banglalink/toffee/ui/verify/VerifyCodeActivity.kt
+++ app/src/main/java/com/banglalink/toffee/ui/verify/VerifyCodeActivity.kt
@@ -2,13 +2,12 @@ package com.banglalink.toffee.ui.verify
 
 import android.graphics.Paint
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.banglalink.toffee.R
+import com.banglalink.toffee.databinding.LayoutLoginConfirmBinding
 import com.banglalink.toffee.extension.launchActivity
 import com.banglalink.toffee.extension.observe
 import com.banglalink.toffee.extension.showToast
@@ -16,6 +15,7 @@ import com.banglalink.toffee.model.Resource
 import com.banglalink.toffee.ui.common.BaseAppCompatActivity
 import com.banglalink.toffee.ui.home.HomeActivity
 import com.banglalink.toffee.ui.widget.VelBoxProgressDialog
+import com.banglalink.toffee.util.unsafeLazy
 
 class VerifyCodeActivity : BaseAppCompatActivity() {
 
@@ -24,28 +24,19 @@ class VerifyCodeActivity : BaseAppCompatActivity() {
         const val REFERRAL_CODE = "REFERRAL_CODE"
     }
 
-    private val phoneNumber by lazy {
+    private val phoneNumber by unsafeLazy {
         intent.getStringExtra(PHONE_NUMBER)
     }
 
-    private val referralCode by lazy {
+    private val referralCode by unsafeLazy {
         intent.getStringExtra(REFERRAL_CODE)
     }
-    private val countdownTV by lazy {
-        findViewById<TextView>(R.id.countdown_tv)
-    }
-    private val resendButton by lazy {
-        findViewById<TextView>(R.id.resend)
-    }
-    private val confirmButton by lazy {
-        findViewById<Button>(R.id.confirm_btn)
-    }
 
-    private val progressDialog by lazy {
+    private val progressDialog by unsafeLazy {
         VelBoxProgressDialog(this)
     }
 
-    private val viewModel by lazy {
+    private val viewModel by unsafeLazy {
         ViewModelProviders.of(this).get(VerifyCodeViewModel::class.java)
     }
 
@@ -53,16 +44,18 @@ class VerifyCodeActivity : BaseAppCompatActivity() {
 
     private var resendBtnPressCount: Int = 0
 
+    private lateinit var binding:LayoutLoginConfirmBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_login_confirm)
+        binding = DataBindingUtil.setContentView(this,R.layout.layout_login_confirm)
 
         val codeEditText = findViewById<EditText>(R.id.code_number)
-        resendButton.paintFlags = resendButton.paintFlags or Paint.UNDERLINE_TEXT_FLAG
-        resendButton.setOnClickListener{
+        binding.resend.paintFlags = binding.resend.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        binding.resend.setOnClickListener{
             handleResendButton()
         }
-        confirmButton.setOnClickListener{
+        binding.confirmBtn.setOnClickListener{
             verifyCode(codeEditText.text.toString())
         }
 
@@ -89,13 +82,13 @@ class VerifyCodeActivity : BaseAppCompatActivity() {
 
     private fun handleResendButton(){
         resendBtnPressCount++
-        resendButton.visibility = View.INVISIBLE
+        binding.resend.visibility = View.INVISIBLE
         startCountDown(if (resendBtnPressCount <= 1) 1 else 30)
 
         viewModel.resendCode(phoneNumber, referralCode)
     }
     private fun startCountDown(countDownTimeInMinute: Int) {
-        countdownTV.visibility = View.VISIBLE
+        binding.countdownTv.visibility = View.VISIBLE
         resendCodeTimer?.cancel()
         resendCodeTimer = ResendCodeTimer(this,countDownTimeInMinute).also { timer ->
             observe(timer.tickLiveData){
@@ -104,14 +97,14 @@ class VerifyCodeActivity : BaseAppCompatActivity() {
                 val seconds = (remainingSecs % 60).toInt()
                 val timeText = (String.format("%02d", minutes)
                         + ":" + String.format("%02d", seconds))
-                countdownTV.text = "Resend option will be activated after $timeText"
+                binding.countdownTv.text = "Resend option will be activated after $timeText"
             }
 
             observe(timer.finishLiveData){
-                resendButton.isEnabled = true
-                resendButton.visibility = View.VISIBLE
-                countdownTV.visibility = View.INVISIBLE
-                countdownTV.text = ""
+                binding.resend.isEnabled = true
+                binding.resend.visibility = View.VISIBLE
+                binding.countdownTv.visibility = View.INVISIBLE
+                binding.countdownTv.text = ""
 
                 timer.finishLiveData.removeObservers(this)
                 timer.tickLiveData.removeObservers(this)
