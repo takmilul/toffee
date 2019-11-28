24e10f8b5acf68bdea196be1d52601ac584d405a
rokibhasan <rokib.buet06@gmail.com>
2019-11-28 17:57:18 +0600

    Snackbar extenstions added

---
+++ app/src/main/java/com/banglalink/toffee/extension/SnackBarExtensions.kt
@@ -0,0 +1,16 @@
+package com.banglalink.toffee.extension
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+
+fun Snackbar.action(action: String, color: Int? = null, listener: (View) -> Unit) {
+    setAction(action, listener)
+    color?.let { setActionTextColor(color) }
+}
+
+inline fun View.snack(message: String, length: Int = Snackbar.LENGTH_INDEFINITE, f: Snackbar.() -> Unit) {
+    val snack = Snackbar.make(this, message, length)
+    snack.f()
+    snack.show()
+}
