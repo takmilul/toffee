38c3aaf805329c0ee52bc53c43aa6711f24b73d6
rokibhasan <rokib.buet06@gmail.com>
2019-12-02 16:24:45 +0600

    Drawer helper is added

---
+++ app/src/main/java/com/banglalink/toffee/ui/home/DrawerHelper.kt
@@ -0,0 +1,340 @@
+package com.banglalink.toffee.ui.home
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.banglalink.toffee.R
+import com.banglalink.toffee.data.storage.Preference
+import com.banglalink.toffee.databinding.ActivityMainMenuBinding
+import com.banglalink.toffee.extension.launchActivity
+import com.banglalink.toffee.extension.loadProfileImage
+import com.banglalink.toffee.extension.observe
+import com.banglalink.toffee.model.*
+import com.banglalink.toffee.ui.about.AboutActivity
+import com.banglalink.toffee.ui.channels.ChannelFragment
+import com.banglalink.toffee.ui.common.HtmlPageViewActivity
+import com.banglalink.toffee.ui.common.ParentLevelAdapter
+import com.banglalink.toffee.ui.favorite.FavoriteFragment
+import com.banglalink.toffee.ui.profile.ViewProfileActivity
+import com.banglalink.toffee.ui.recent.RecentFragment
+import com.banglalink.toffee.ui.refer.ReferAFriendActivity
+import com.banglalink.toffee.ui.settings.SettingsActivity
+import com.banglalink.toffee.ui.subscription.PackageListActivity
+import kotlinx.android.synthetic.main.layout_appbar.view.*
+import java.util.ArrayList
+
+class DrawerHelper(val activity: HomeActivity,val binding:ActivityMainMenuBinding): ParentLevelAdapter.OnNavigationItemClickListener{
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    fun initDrawer() {
+        toggle = ActionBarDrawerToggle(
+            activity,
+            binding.drawerLayout,
+            binding.tbar.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        //After instantiating your ActionBarDrawerToggle
+        toggle.isDrawerIndicatorEnabled = false
+        toggle.setHomeAsUpIndicator(R.drawable.ic_home)
+        val parentAdapter =
+            ParentLevelAdapter(activity, generateNavMenu(), this, binding.navMenuList)
+        binding.navMenuList.setAdapter(parentAdapter)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        toggle.toolbarNavigationClickListener = View.OnClickListener {
+            if (activity.supportFragmentManager.backStackEntryCount > 1) {
+                activity.supportFragmentManager.popBackStack(LandingPageFragment::class.java.name, 0)
+            }
+        }
+
+        setNameAndCredit()
+        setProfileInfo()
+    }
+
+    private fun setNameAndCredit() {
+        val header = binding.navView.getHeaderView(0)
+        (header.findViewById<View>(R.id.credit_tv) as TextView).text =
+            "Credit: " + Preference.getInstance().balance
+        (header.findViewById<View>(R.id.customer_name_tv) as TextView).text =
+            Preference.getInstance().customerName
+        header.findViewById<View>(R.id.nav_bar_close).setOnClickListener {
+            if (binding.drawerLayout != null) {
+                binding.drawerLayout.closeDrawer(GravityCompat.END)
+            }
+        }
+    }
+
+    private fun setProfileInfo() {
+        val header = binding.navView.getHeaderView(0)
+        val profileName = header.findViewById(R.id.profile_name) as TextView
+        if(!Preference.getInstance().customerName.isBlank()){
+            profileName.text= Preference.getInstance().customerName
+        }
+        val profilePicture = header.findViewById(R.id.profile_picture) as ImageView
+
+        activity.observe(Preference.getInstance().profileImageUrlLiveData){
+            profilePicture.loadProfileImage(it)
+        }
+
+        profilePicture.setOnClickListener{
+            activity.launchActivity<ViewProfileActivity>()
+        }
+        profileName.setOnClickListener{
+            activity.launchActivity<ViewProfileActivity>()
+        }
+    }
+
+    fun generateNavMenu(): List<NavigationMenu> {
+        val navigationMenuList = ArrayList<NavigationMenu>()
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_CHANNEL,
+                "TV Channels",
+                R.mipmap.ic_menu_channels,
+                ArrayList(),
+                true
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_RECENT,
+                "Recent",
+                R.mipmap.ic_menu_recent,
+                ArrayList()
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_FAV,
+                "Favorites",
+                R.mipmap.ic_menu_favorites,
+                ArrayList()
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_SUB_VIDEO,
+                activity.getString(R.string.menu_create_text),
+                R.mipmap.ic_menu_create,
+                ArrayList(),
+                true
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_SUBSCIPTIONS,
+                "Subscriptions",
+                R.mipmap.ic_menu_subscriptions,
+                ArrayList(),
+                true
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_INVITE_FRIEND,
+                activity.getString(R.string.invite_friends_txt),
+                R.mipmap.ic_menu_invite,
+                ArrayList()
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_SETTINGS,
+                "Settings",
+                R.mipmap.ic_menu_settings,
+                ArrayList()
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_ABOUT,
+                "About",
+                R.mipmap.ic_menu_about,
+                ArrayList()
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_FAQ,
+                activity.getString(R.string.menu_faqs_text),
+                R.drawable.ic_menu_faq,
+                ArrayList()
+            )
+        )
+        navigationMenuList.add(
+            NavigationMenu(
+                ID_LOGOUT,
+                "Logout",
+                R.mipmap.ic_menu_exit,
+                ArrayList()
+            )
+        )
+
+
+        return navigationMenuList
+    }
+
+    override fun onMenuClick(menu: NavigationMenu?) {
+        when (menu?.id) {
+            ID_VIDEO -> {
+                val currentFragment = getCurrentContentFragment()
+                if (CatchupFragment::class.java.name != currentFragment!!.tag) {
+                    activity.loadFragmentById( R.id.content_viewer,
+                        CatchupFragment.createInstance(0, 0, "", "All Videos", menu.name, "VOD")
+                        ,CatchupFragment::class.java.name
+                    )
+                } else {
+                    val catchupFragment = currentFragment as CatchupFragment
+                    catchupFragment.updateInfo(0, 0, "", "All Videos", menu.name, "VOD")
+                }
+
+                binding.drawerLayout.closeDrawers()
+                activity.minimizePlayer()
+            }
+            ID_RECENT -> {
+                val currentFragment = getCurrentContentFragment()
+                if (currentFragment !is RecentFragment) {
+                    activity.loadFragmentById(
+                        R.id.content_viewer, RecentFragment(),
+                        RecentFragment::class.java.name
+                    )
+                }
+                binding.drawerLayout.closeDrawers()
+                activity.minimizePlayer()
+            }
+            ID_CHANNEL -> {
+                val currentFragment = getCurrentContentFragment()
+                if (currentFragment !is ChannelFragment) {
+                    activity.loadFragmentById( R.id.content_viewer, ChannelFragment.createInstance(
+                        0,
+                        "",
+                        activity.getString(R.string.menu_channel_text)
+                    ), ChannelFragment::class.java.getName())
+                }
+                binding.drawerLayout.closeDrawers()
+                activity.minimizePlayer()
+            }
+            ID_FAV -> {
+                val currentFragment = getCurrentContentFragment()
+                if (currentFragment !is FavoriteFragment) {
+                    activity.loadFragmentById(
+                        R.id.content_viewer, FavoriteFragment(),
+                        FavoriteFragment::class.java.getName()
+                    )
+                }
+                binding.drawerLayout.closeDrawers()
+                activity.minimizePlayer()
+            }
+            ID_SUB_VIDEO -> {
+                activity.launchActivity<HtmlPageViewActivity> {
+                    putExtra(
+                        HtmlPageViewActivity.TITLE_KEY,
+                        activity.getString(R.string.menu_create_text)
+                    )
+                    putExtra(HtmlPageViewActivity.CONTENT_KEY, MICRO_SITE_URL)
+                }
+                binding.drawerLayout.closeDrawers()
+            }
+            ID_SUBSCIPTIONS->{
+                activity.launchActivity<PackageListActivity>()
+            }
+            ID_SETTINGS -> {
+                activity.launchActivity<SettingsActivity>()
+                binding.drawerLayout.closeDrawers()
+
+            }
+            ID_ABOUT -> {
+                activity.launchActivity<AboutActivity>()
+                binding.drawerLayout.closeDrawers()
+
+            }
+            ID_FAQ -> {
+                activity.launchActivity<HtmlPageViewActivity> {
+                    putExtra(HtmlPageViewActivity.CONTENT_KEY, FAQ_URL)
+                    putExtra(HtmlPageViewActivity.TITLE_KEY, activity.getString(R.string.menu_faqs_text))
+                }
+                binding.drawerLayout.closeDrawers()
+
+            }
+            ID_LOGOUT->{
+                activity.handleExitApp()
+            }
+            ID_INVITE_FRIEND->{
+                activity.launchActivity<ReferAFriendActivity>()
+                binding.drawerLayout.closeDrawers()
+            }
+        }
+    }
+
+    private fun getCurrentContentFragment(): Fragment? {
+        return activity.supportFragmentManager.findFragmentById(R.id.content_viewer)
+    }
+
+    override fun onSubCategoryClick(
+        subcategory: NavSubcategory?,
+        category: NavCategory?,
+        parent: NavigationMenu?
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCategoryClick(category: NavCategory, parent: NavigationMenu) {
+        if (parent.id == ID_VIDEO) run {
+            val currentFragment = getCurrentContentFragment()
+            if (CatchupFragment::class.java.name != currentFragment!!.tag) {
+                activity.loadFragmentById(R.id.content_viewer,CatchupFragment.createInstance(
+                    category.id,
+                    0,
+                    "",
+                    parent.name,
+                    category.categoryName,
+                    "VOD"
+                ), CatchupFragment::class.java.name)
+            } else {
+                val catchupFragment = currentFragment as CatchupFragment
+                catchupFragment.updateInfo(
+                    category.id,
+                    0,
+                    "",
+                    parent.name,
+                    category.categoryName,
+                    "VOD"
+                )
+            }
+
+            binding.drawerLayout.closeDrawers()
+            activity.minimizePlayer()
+        }
+    }
+
+    fun updateAdapter(navCategoryList:List<NavCategory>) {
+        val parentAdapter = ParentLevelAdapter(
+            activity,
+            generateNavMenu(),
+            this,
+            binding.navMenuList
+        )
+        binding.navMenuList.setAdapter(parentAdapter)
+        var menu: NavigationMenu
+        try {
+            menu = NavigationMenu(
+                ID_VIDEO,
+                "All Videos",
+                R.mipmap.ic_menu_vod,
+                navCategoryList
+            )
+            parentAdapter.insert(menu, 1)
+            binding.navMenuList.expandGroup(1)
+        } catch (e: Exception) {
+            menu = NavigationMenu(ID_VOD, "VoD", R.mipmap.ic_menu_vod, ArrayList())
+            parentAdapter.insert(menu, 3)
+        }
+    }
+}
