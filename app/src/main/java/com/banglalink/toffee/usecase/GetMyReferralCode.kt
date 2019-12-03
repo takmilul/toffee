cda8454ee1af9e1f3ff9c59bf3f557f8b52f51ce
rokibhasan <rokib.buet06@gmail.com>
2019-12-03 18:02:56 +0600

    Return type is now ReferralCodeBean

--- app/src/main/java/com/banglalink/toffee/usecase/GetMyReferralCode.kt
+++ app/src/main/java/com/banglalink/toffee/usecase/GetMyReferralCode.kt
@@ -4,13 +4,14 @@ import com.banglalink.toffee.data.network.request.ReferralCodeRequest
 import com.banglalink.toffee.data.network.retrofit.ToffeeApi
 import com.banglalink.toffee.data.network.util.tryIO
 import com.banglalink.toffee.data.storage.Preference
+import com.banglalink.toffee.model.ReferralCodeBean
 
 class GetMyReferralCode(private val preference: Preference,private val toffeeApi: ToffeeApi) {
 
-    suspend fun execute():String{
+    suspend fun execute():ReferralCodeBean{
         val response = tryIO{
             toffeeApi.getMyReferralCode(ReferralCodeRequest(preference.customerId,preference.password))
         }
-        return response.response.referralCode
+        return response.response
     }
 }
