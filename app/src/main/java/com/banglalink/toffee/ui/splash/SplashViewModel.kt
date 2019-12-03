96695e75e89f5a5f0ab1fffee55f3b7d05a3ae30
rokibhasan <rokib.buet06@gmail.com>
2019-12-03 17:58:48 +0600

    ResultLiveData is added

--- app/src/main/java/com/banglalink/toffee/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/banglalink/toffee/ui/splash/SplashViewModel.kt
@@ -1,31 +1,20 @@
 package com.banglalink.toffee.ui.splash
 
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.LiveData
 import com.banglalink.toffee.BuildConfig
 import com.banglalink.toffee.data.network.retrofit.RetrofitApiClient
+import com.banglalink.toffee.data.network.util.resultLiveData
 import com.banglalink.toffee.data.storage.Preference
-import com.banglalink.toffee.exception.UpdateRequiredException
-import com.banglalink.toffee.extension.setError
-import com.banglalink.toffee.extension.setSuccess
-import com.banglalink.toffee.extension.toLiveData
+import com.banglalink.toffee.model.Customer
 import com.banglalink.toffee.model.Resource
 import com.banglalink.toffee.ui.common.BaseViewModel
 import com.banglalink.toffee.usecase.ApiLogin
 import com.banglalink.toffee.usecase.CheckUpdate
 import com.banglalink.toffee.usecase.GetProfile
-import com.banglalink.toffee.util.getError
 import com.banglalink.toffee.util.unsafeLazy
-import kotlinx.coroutines.launch
 
 class SplashViewModel(application: Application) : BaseViewModel(application) {
-
-    private val customerLoginMutableLiveData = MutableLiveData<Resource<Boolean>>()
-    val customerLoginLiveData = customerLoginMutableLiveData.toLiveData()
-
-    private val updateRequiredMutableLiveData = MutableLiveData<UpdateRequiredException>()
-    val updateRequiredLiveData = updateRequiredMutableLiveData.toLiveData()
 
     private val checkUpdate by unsafeLazy {
         CheckUpdate(RetrofitApiClient.authApi)
@@ -38,36 +27,15 @@ class SplashViewModel(application: Application) : BaseViewModel(application) {
         GetProfile(Preference.getInstance(),RetrofitApiClient.toffeeApi)
     }
 
-    init {
-        init(false)
-    }
-
-    fun init(skipUpdate:Boolean = false){
-
-        viewModelScope.launch {
-            try{
-                if(!skipUpdate){
-                    checkUpdate.execute(BuildConfig.VERSION_CODE.toString())
-                }
-                if(isCustomerLoggedIn()){
-                    apiLogin.execute()//auto login
-                    getProfile.execute()//fetch profile
-                    customerLoginMutableLiveData.setSuccess(true)
-                }else{
-                    customerLoginMutableLiveData.setSuccess(false)
-                }
+    fun init(skipUpdate:Boolean = false):LiveData<Resource<Customer>>{
+        return resultLiveData {
+            if(!skipUpdate){
+                checkUpdate.execute(BuildConfig.VERSION_CODE.toString())
             }
-            catch (e:Exception){
-                when (e) {
-                    is UpdateRequiredException -> {
-                        updateRequiredMutableLiveData.postValue(e)
-                    }
-                    else -> customerLoginMutableLiveData.setError(getError(e))
-                }
-            }
-
+            apiLogin.execute()
+            getProfile.execute()
         }
     }
 
-    private fun isCustomerLoggedIn()=Preference.getInstance().customerId != 0
+    fun isCustomerLoggedIn()=Preference.getInstance().customerId != 0
 }
