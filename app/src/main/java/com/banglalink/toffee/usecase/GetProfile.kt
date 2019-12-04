aea7db0606a3a579ffcc692318a61ac32d7e2f13
rokibhasan <rokib.buet06@gmail.com>
2019-12-04 17:22:22 +0600

    Save customer name in preference

--- app/src/main/java/com/banglalink/toffee/usecase/GetProfile.kt
+++ app/src/main/java/com/banglalink/toffee/usecase/GetProfile.kt
@@ -12,6 +12,9 @@ class GetProfile(private val preference: Preference,private val toffeeApi: Toffe
         val response = tryIO { toffeeApi.getCustomerProfile(ProfileRequest(preference.customerId,preference.password)) }
         preference.userImageUrl = response.response.customer.profile.photoUrl?:""
         preference.balance = response.response.balance
+        response.response.customer.profile.name?.let {
+            preference.customerName = it
+        }
         return response.response.customer
     }
 }
