f7db9b1ff08f3610dbe9e16161f939ab977f8721
rokibhasan <rokib.buet06@gmail.com>
2019-12-05 17:17:12 +0600

    Member liveData removed Coroutine LiveData used

--- app/src/main/java/com/banglalink/toffee/ui/verify/VerifyCodeViewModel.kt
+++ app/src/main/java/com/banglalink/toffee/ui/verify/VerifyCodeViewModel.kt
@@ -1,26 +1,19 @@
 package com.banglalink.toffee.ui.verify
 
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.LiveData
 import com.banglalink.toffee.data.network.retrofit.RetrofitApiClient
+import com.banglalink.toffee.data.network.util.resultLiveData
 import com.banglalink.toffee.data.storage.Preference
-import com.banglalink.toffee.extension.setError
-import com.banglalink.toffee.extension.setSuccess
-import com.banglalink.toffee.extension.toLiveData
+import com.banglalink.toffee.model.Customer
 import com.banglalink.toffee.model.Resource
 import com.banglalink.toffee.ui.common.BaseViewModel
 import com.banglalink.toffee.usecase.GetProfile
 import com.banglalink.toffee.usecase.SigninByPhone
 import com.banglalink.toffee.usecase.VerifyCode
-import com.banglalink.toffee.util.getError
 import com.banglalink.toffee.util.unsafeLazy
-import kotlinx.coroutines.launch
 
 class VerifyCodeViewModel(application: Application) : BaseViewModel(application) {
-
-    private val verifyCodeMutableLiveData = MutableLiveData<Resource<Boolean>>()
-    val verifyCodeLiveData = verifyCodeMutableLiveData.toLiveData()
 
     private val getProfile: GetProfile by unsafeLazy {
         GetProfile(Preference.getInstance(), RetrofitApiClient.toffeeApi)
@@ -34,26 +27,16 @@ class VerifyCodeViewModel(application: Application) : BaseViewModel(application)
         SigninByPhone(Preference.getInstance(), RetrofitApiClient.toffeeApi)
     }
 
-    fun verifyCode(code: String) {
-        viewModelScope.launch {
-            try {
-                verifyCode.execute(code)
-                getProfile.execute()
-                verifyCodeMutableLiveData.setSuccess(true)
-            } catch (e: Exception) {
-                verifyCodeMutableLiveData.setError(getError(e))
-            }
-
+    fun verifyCode(code: String) :LiveData<Resource<Customer>>{
+        return resultLiveData {
+            verifyCode.execute(code)
+            getProfile.execute()
         }
     }
 
-    fun resendCode(phoneNumber: String, referralCode: String) {
-        viewModelScope.launch {
-            try {
-                signinByPhone.execute(phoneNumber, referralCode)
-            } catch (e: Exception) {
-                verifyCodeMutableLiveData.setError(getError(e))
-            }
+    fun resendCode(phoneNumber: String, referralCode: String):LiveData<Resource<Unit>> {
+        return resultLiveData {
+            signinByPhone.execute(phoneNumber,referralCode)
         }
     }
 }
