2fb00566069b36e0c43a16542a2e7a9b330670e3
rokibhasan <rokib.buet06@gmail.com>
2019-12-06 18:21:58 +0600

    Message passed to super class

--- app/src/main/java/com/banglalink/toffee/exception/UpdateRequiredException.kt
+++ app/src/main/java/com/banglalink/toffee/exception/UpdateRequiredException.kt
@@ -2,4 +2,4 @@ package com.banglalink.toffee.exception
 
 import java.lang.Exception
 
-class UpdateRequiredException(val title:String,val updateMsg:String, val forceUpdate:Boolean):Exception()
+class UpdateRequiredException(val title:String,val updateMsg:String, val forceUpdate:Boolean):Exception(updateMsg)
