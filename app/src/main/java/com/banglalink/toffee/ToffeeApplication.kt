def5078b77ae58405b709be2bb3de990cb76e51e
rokibhasan <rokib.buet06@gmail.com>
2020-03-22 18:07:57 +0600

    Network connectivity observed in application

--- app/src/main/java/com/banglalink/toffee/ToffeeApplication.kt
+++ app/src/main/java/com/banglalink/toffee/ToffeeApplication.kt
@@ -1,9 +1,13 @@
 package com.banglalink.toffee
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkRequest
 import coil.Coil
 import coil.ImageLoader
 import coil.util.CoilUtils
+import com.banglalink.toffee.analytics.HeartBeatManager
 import com.banglalink.toffee.data.storage.Preference
 import okhttp3.OkHttpClient
 
@@ -26,8 +30,17 @@ class ToffeeApplication : Application() {
 
         }
 
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.registerNetworkCallback(NetworkRequest.Builder().build(), HeartBeatManager)
+
         Coil.setDefaultImageLoader(imageLoader)
 
+    }
+
+    override fun onTerminate() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.unregisterNetworkCallback(HeartBeatManager)
+        super.onTerminate()
     }
 
     override fun onTrimMemory(level: Int) {
