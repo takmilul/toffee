dc726d7d444c1766d19d642e49ebc7d118edec56
rokibhasan <rokib.buet06@gmail.com>
2020-03-22 18:09:14 +0600

    Databinding and snackbar added

--- app/src/main/java/com/banglalink/toffee/ui/login/SigninByPhoneActivity.kt
+++ app/src/main/java/com/banglalink/toffee/ui/login/SigninByPhoneActivity.kt
@@ -14,15 +14,11 @@ import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.TextView
-import androidx.constraintlayout.widget.Group
-import androidx.lifecycle.ViewModelProviders
+import androidx.databinding.DataBindingUtil
 import com.banglalink.toffee.R
-import com.banglalink.toffee.extension.launchActivity
-import com.banglalink.toffee.extension.observe
-import com.banglalink.toffee.extension.showToast
+import com.banglalink.toffee.databinding.ActivitySigninByPhoneBinding
+import com.banglalink.toffee.extension.*
 import com.banglalink.toffee.model.INVALID_REFERRAL_ERROR_CODE
 import com.banglalink.toffee.model.Resource
 import com.banglalink.toffee.model.TERMS_AND_CONDITION_URL
@@ -35,39 +31,33 @@ import com.banglalink.toffee.util.unsafeLazy
 
 class SigninByPhoneActivity : BaseAppCompatActivity() {
 
+    lateinit var binding :ActivitySigninByPhoneBinding
+
     private val viewModel by unsafeLazy {
-        ViewModelProviders.of(this).get(SigninByPhoneViewModel::class.java)
-    }
-    private val phoneNumber by unsafeLazy {
-        findViewById<EditText>(R.id.phone_number_et)
+       getViewModel<SigninByPhoneViewModel>()
     }
 
     private val progressDialog by unsafeLazy {
         VelBoxProgressDialog(this)
     }
 
-    private lateinit var referralCodeEt: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin_by_phone)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_signin_by_phone)
 
-        referralCodeEt = findViewById(R.id.ref_code_et)
         setSpannableTermsAndConditions()
 
-        val loginBtn = findViewById<Button>(R.id.login_btn)
-        loginBtn.setOnClickListener {
+        binding.loginBtn.setOnClickListener {
             handleLogin()
         }
-        val termsAndConditionCheckBox = findViewById<CheckBox>(R.id.checkBox)
-        termsAndConditionCheckBox.setOnClickListener {
-            loginBtn.isEnabled = termsAndConditionCheckBox.isChecked
+        binding.termsAndConditionsCheckbox.setOnClickListener {
+            binding.loginBtn.isEnabled = binding.termsAndConditionsCheckbox.isChecked
         }
 
     }
 
     private fun handleLogin() {
-        var phoneNo = phoneNumber.text.toString()
+        var phoneNo = binding.phoneNumberEt.text.toString()
 
         if (TextUtils.isEmpty(phoneNo)) {
             showAlertDialog(this, getString(R.string.phone_no_required_title), "")
@@ -84,15 +74,15 @@ class SigninByPhoneActivity : BaseAppCompatActivity() {
 
 
         progressDialog.show()
-        this.phoneNumber.setText(phoneNo)
+        binding.phoneNumberEt.setText(phoneNo)
 
-        observe(viewModel.signIn(phoneNo, referralCodeEt.text.toString())) {
+        observe(viewModel.signIn(phoneNo, binding.refCodeEt.text.toString())) {
             progressDialog.dismiss()
             when (it) {
                 is Resource.Success -> {
                     launchActivity<VerifyCodeActivity> {
-                        putExtra(VerifyCodeActivity.PHONE_NUMBER, phoneNumber.text.toString())
-                        putExtra(VerifyCodeActivity.REFERRAL_CODE, referralCodeEt.text.toString())
+                        putExtra(VerifyCodeActivity.PHONE_NUMBER, binding.phoneNumberEt.text.toString())
+                        putExtra(VerifyCodeActivity.REFERRAL_CODE, binding.refCodeEt.text.toString())
                         putExtra(VerifyCodeActivity.REG_SESSION_TOKEN, it.data)
                     }
                     finish()
@@ -103,7 +93,11 @@ class SigninByPhoneActivity : BaseAppCompatActivity() {
                             showInvalidReferralCodeDialog(it.error.msg, it.error.additionalMsg)
                         }
                         else -> {
-                            showToast(it.error.msg)
+                            binding.root.snack(it.error.msg){
+                                action("Retry") {
+                                   handleLogin()
+                                }
+                            }
                         }
                     }
 
@@ -136,7 +130,7 @@ class SigninByPhoneActivity : BaseAppCompatActivity() {
         (alertView.findViewById<View>(R.id.title) as TextView).text = referralStatusMessage
         alertView.findViewById<View>(R.id.continue_btn)
             .setOnClickListener {
-                referralCodeEt.setText("")
+                binding.refCodeEt.setText("")
                 handleLogin()
                 alertDialog.dismiss()
             }
@@ -146,8 +140,8 @@ class SigninByPhoneActivity : BaseAppCompatActivity() {
     }
 
     fun handleHaveReferralOption(view: View) {
-        findViewById<Group>(R.id.group).visibility = View.VISIBLE
-        findViewById<Group>(R.id.group_have_ref).visibility = View.GONE
+        binding.group.visibility = View.VISIBLE
+        binding.groupHaveRef.visibility = View.GONE
     }
 
     private fun setSpannableTermsAndConditions() {
@@ -173,9 +167,8 @@ class SigninByPhoneActivity : BaseAppCompatActivity() {
         }
         ss.setSpan(clickableSpan, 15, ss.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-        val textView = findViewById<TextView>(R.id.terms_and_conditions_tv)
-        textView.text = ss
-        textView.movementMethod = LinkMovementMethod.getInstance()
+        binding.termsAndConditionsTv.text = ss
+        binding.termsAndConditionsTv.movementMethod = LinkMovementMethod.getInstance()
 
     }
 }
