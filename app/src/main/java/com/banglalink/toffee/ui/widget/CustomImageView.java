78a6739954b0e14348a1b1708ea45c03fdad86e5
rokibhasan <rokib.buet06@gmail.com>
2020-06-26 16:40:36 +0600

    Unnecessary import removed
    (cherry picked from commit 8164187433c42408415e7a01c681f6f75e610322)

--- app/src/main/java/com/banglalink/toffee/ui/widget/CustomImageView.java
+++ app/src/main/java/com/banglalink/toffee/ui/widget/CustomImageView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-import pl.droidsonroids.gif.GifImageView;
 
 public class CustomImageView extends ImageView {
     public CustomImageView(Context context) {
