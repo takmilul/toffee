4e684a25a4858e200ad0a6820d41f63537945230
rokibhasan <rokib.buet06@gmail.com>
2020-07-16 11:34:51 +0600

    Wrapped in try catch

--- app/src/main/java/com/banglalink/toffee/receiver/SMSBroadcastReceiver.kt
+++ app/src/main/java/com/banglalink/toffee/receiver/SMSBroadcastReceiver.kt
@@ -5,13 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.banglalink.toffee.analytics.ToffeeAnalytics
 import com.banglalink.toffee.extension.toLiveData
 import com.banglalink.toffee.util.SingleLiveEvent
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
-import java.util.regex.Matcher
-import java.util.regex.Pattern
+import java.lang.Exception
 
 class SMSBroadcastReceiver : BroadcastReceiver() {
 
@@ -20,26 +20,31 @@ class SMSBroadcastReceiver : BroadcastReceiver() {
 
     private val TAG = "SMSBroadcastReceiver"
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d(TAG, "onReceive: ")
-        if (SmsRetriever.SMS_RETRIEVED_ACTION == intent?.action) {
-            val extras: Bundle? = intent?.extras
-            val mStatus =
-                extras?.get(SmsRetriever.EXTRA_STATUS) as Status?
-            when (mStatus!!.statusCode) {
-                CommonStatusCodes.SUCCESS -> {
-                    // Get SMS message contents'
-                    val message =
-                        extras?.get(SmsRetriever.EXTRA_SMS_MESSAGE) as String?
-                    Log.d(TAG, "onReceive: failure $message")
-                    val start = message!!.indexOf(":") + 1;
-                    val otp = message!!.substring(start, start + 6)
-                    _otpLiveData.postValue(otp)
-                }
-                CommonStatusCodes.TIMEOUT -> {
-                    // Waiting for SMS timed out (5 minutes)
-                    Log.d(TAG, "onReceive: failure")
+        try {
+            if (SmsRetriever.SMS_RETRIEVED_ACTION == intent?.action) {
+                val extras: Bundle? = intent?.extras
+                val mStatus =
+                    extras?.get(SmsRetriever.EXTRA_STATUS) as Status?
+                when (mStatus!!.statusCode) {
+                    CommonStatusCodes.SUCCESS -> {
+                        // Get SMS message contents'
+                        val message =
+                            extras?.get(SmsRetriever.EXTRA_SMS_MESSAGE) as String?
+                        Log.d(TAG, "onReceive: failure $message")
+                        val start = message!!.indexOf(":") + 1;
+                        val otp = message!!.substring(start, start + 6)
+                        _otpLiveData.postValue(otp)
+                    }
+                    CommonStatusCodes.TIMEOUT -> {
+                        // Waiting for SMS timed out (5 minutes)
+                        Log.d(TAG, "onReceive: failure")
+                    }
                 }
             }
+        }catch (e:Exception){
+            ToffeeAnalytics.logException(e)
+            ToffeeAnalytics.logBreadCrumb("Error in sms broadcast receiver")
         }
+
     }
 }
