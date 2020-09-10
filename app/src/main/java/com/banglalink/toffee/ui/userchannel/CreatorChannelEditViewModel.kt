e3158316a914c0719d1c9cf42516060504314f7f
Takmilul Ehsan <takmilul.ehsan@nexdecade.com>
2020-09-10 19:24:57 +0600

    some problem solved

--- app/src/main/java/com/banglalink/toffee/ui/userchannel/CreatorChannelEditViewModel.kt
+++ app/src/main/java/com/banglalink/toffee/ui/userchannel/CreatorChannelEditViewModel.kt
@@ -1,25 +1,11 @@
 package com.banglalink.toffee.ui.userchannel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.banglalink.toffee.data.network.retrofit.RetrofitApiClient
-import com.banglalink.toffee.data.network.util.resultLiveData
-import com.banglalink.toffee.data.storage.Preference
-import com.banglalink.toffee.model.Resource
 import com.banglalink.toffee.model.UserChannel
-import com.banglalink.toffee.usecase.EditChannel
-import com.banglalink.toffee.util.unsafeLazy
 
 class CreatorChannelEditViewModel: ViewModel() {
     
     val userChannel = MutableLiveData<UserChannel>()
-    private val editChannel by unsafeLazy { EditChannel(Preference.getInstance(), RetrofitApiClient.toffeeApi) }
-    
-    fun getEditInfo(): LiveData<Resource<UserChannel>>{
-        return resultLiveData { 
-            editChannel.execute()
-        }
-    }
     
 }
--- app/src/main/java/com/banglalink/toffee/usecase/GetChannelInfo.kt
+++ app/src/main/java/com/banglalink/toffee/usecase/GetChannelInfo.kt
@@ -7,13 +7,13 @@ import com.banglalink.toffee.data.storage.Preference
 import com.banglalink.toffee.model.UserChannel
 import com.banglalink.toffee.util.Utils
 
-class EditChannel(private val preference: Preference, private val toffeeApi: ToffeeApi) {
+class GetChannelInfo(private val preference: Preference, private val toffeeApi: ToffeeApi) {
     
     var mOffset: Int = 0
         private set
     private val limit = 10
     
-    suspend fun execute(): UserChannel {
+    suspend fun execute(): UserChannel? {
         val channelCategoryList = listOf("Movie","Natok","Music","Video")
         val subscriptionPriceList = listOf("৳50","৳100","৳150","৳200","৳250")
         
@@ -36,10 +36,6 @@ class EditChannel(private val preference: Preference, private val toffeeApi: Tof
             }
             return UserChannel(channel.id, channel.poster_url_mobile, channel.poster_url_mobile, channel.program_name, channel.description, channelCategoryList, channelCategoryList[0], subscriptionPriceList, subscriptionPriceList[0])
         }
-        return UserChannel("1", "", "", "National Geography", "this is a general description for user channel", channelCategoryList, channelCategoryList[0], subscriptionPriceList, subscriptionPriceList[0])
-    }
-    
-    suspend fun saveChanges(){
-        
+        return null
     }
 }
