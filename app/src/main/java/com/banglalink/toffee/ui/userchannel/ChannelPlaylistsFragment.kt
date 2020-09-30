33792df7903accae43a1844779d57ee7a1569b73
Takmilul Ehsan <takmilul.ehsan@nexdecade.com>
2020-09-30 16:50:49 +0600

    channel playlist dialog implemented

--- app/src/main/java/com/banglalink/toffee/ui/userchannel/ChannelPlaylistsFragment.kt
+++ app/src/main/java/com/banglalink/toffee/ui/userchannel/ChannelPlaylistsFragment.kt
@@ -1,14 +1,20 @@
 package com.banglalink.toffee.ui.userchannel
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.PopupMenu
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.banglalink.toffee.R
+import com.banglalink.toffee.R.layout
 import com.banglalink.toffee.model.ChannelInfo
 import com.banglalink.toffee.ui.common.SingleListFragmentV2
 import com.banglalink.toffee.ui.common.SingleListItemCallback
+import kotlinx.android.synthetic.main.alert_dialog_create_playlist.view.*
+import kotlinx.android.synthetic.main.alert_dialog_create_playlist.view.dialogTitleTextView
+import kotlinx.android.synthetic.main.alert_dialog_decision.view.*
 
 class ChannelPlaylistsFragment : SingleListFragmentV2<ChannelInfo>(), SingleListItemCallback<ChannelInfo> {
 
@@ -34,7 +40,8 @@ class ChannelPlaylistsFragment : SingleListFragmentV2<ChannelInfo>(), SingleList
 
     override fun onItemClicked(item: ChannelInfo) {
         super.onItemClicked(item)
-        findNavController().navigate(R.id.action_menu_channel_to_channelPlaylistVideosFragment)
+        showCreatePlaylistDialog()
+        //findNavController().navigate(R.id.action_menu_channel_to_channelPlaylistVideosFragment)
     }
 
     override fun getEmptyViewInfo(): Pair<Int, String?> {
@@ -49,15 +56,52 @@ class ChannelPlaylistsFragment : SingleListFragmentV2<ChannelInfo>(), SingleList
             setOnMenuItemClickListener {
                 when (it.itemId) {
                     R.id.menu_edit_playlist -> {
-
+                        showEditPlaylistDialog()
                     }
                     R.id.menu_delete_playlist -> {
-
+                        showDeletePlaylistDialog()
                     }
                 }
                 return@setOnMenuItemClickListener true
             }
             show()
+        }
+    }
+    
+    private fun showCreatePlaylistDialog() {
+        val dialogView: View = this.layoutInflater.inflate(layout.alert_dialog_create_playlist, null)
+        val dialogBuilder = AlertDialog.Builder(requireContext()).setView(dialogView)
+        val alertDialog: AlertDialog = dialogBuilder.create().apply {
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            show()
+        }
+        dialogView.submitButton.setOnClickListener { alertDialog.dismiss() }
+    }
+    
+    private fun showEditPlaylistDialog() {
+        val dialogView: View = this.layoutInflater.inflate(layout.alert_dialog_create_playlist, null)
+        val dialogBuilder = AlertDialog.Builder(requireContext()).setView(dialogView)
+        val alertDialog: AlertDialog = dialogBuilder.create().apply {
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            show()
+        }
+        with(dialogView){
+            dialogTitleTextView.text = "Edit Playlist"
+            submitButton.text = "Save"
+            submitButton.setOnClickListener { alertDialog.dismiss() }
+        }
+    }
+    
+    private fun showDeletePlaylistDialog(){
+        val dialogView: View = this.layoutInflater.inflate(layout.alert_dialog_decision, null)
+        val dialogBuilder = AlertDialog.Builder(requireContext()).setView(dialogView)
+        val alertDialog: AlertDialog = dialogBuilder.create().apply {
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            show()
+        }
+        with(dialogView){
+            noButton.setOnClickListener { alertDialog.dismiss() }
+            deleteButton.setOnClickListener { alertDialog.dismiss() }
         }
     }
 }
