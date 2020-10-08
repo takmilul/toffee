0e17edfeab6b1437ad931af2eac1171241514284
rokibhasan <rokib.buet06@gmail.com>
2020-10-08 13:04:25 +0600

    Migration crash bug fix

--- app/src/main/java/com/banglalink/toffee/data/storage/Migraton.kt
+++ app/src/main/java/com/banglalink/toffee/data/storage/Migraton.kt
@@ -5,8 +5,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("CREATE TABLE `channel_view_count` (`channel_id` INTEGER,`view_count` INTEGER, " +
-                "PRIMARY KEY(`channel_id`))")
-        database.execSQL("CREATE UNIQUE INDEX index_channel_view_count_channel_id ON channel_view_count(channel_id)");
+        database.execSQL("CREATE TABLE IF NOT EXISTS `channel_view_count` (`channel_id` INTEGER NOT NULL, `view_count` INTEGER NOT NULL, PRIMARY KEY(`channel_id`))")
+        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS `index_channel_view_count_channel_id` ON `channel_view_count` (`channel_id`)")
     }
 }
