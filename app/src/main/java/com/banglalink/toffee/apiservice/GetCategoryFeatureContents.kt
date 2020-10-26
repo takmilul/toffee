90cab14598fe27f848de9053b31e63612633ba01
Shaiful <shaiful.cse@gmail.com>
2020-10-26 19:15:19 +0600

    Added missing file.

---
+++ app/src/main/java/com/banglalink/toffee/apiservice/GetCategoryFeatureContents.kt
@@ -0,0 +1,40 @@
+package com.banglalink.toffee.apiservice
+
+import com.banglalink.toffee.data.network.request.UgcFeatureContentRequest
+import com.banglalink.toffee.data.network.retrofit.ToffeeApi
+import com.banglalink.toffee.data.network.util.tryIO2
+import com.banglalink.toffee.data.storage.Preference
+import com.banglalink.toffee.model.FeatureContentBean
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+
+class GetCategoryFeatureContents @AssistedInject constructor(
+    private val preference: Preference,
+    private val toffeeApi: ToffeeApi,
+    @Assisted private val requestParams: ApiCategoryRequestParams
+) {
+
+    suspend operator fun invoke(): FeatureContentBean {
+        val request =  UgcFeatureContentRequest(
+            preference.customerId,
+            preference.password
+        )
+
+        val response = tryIO2 {
+            toffeeApi.getUgcFeatureContents(
+                requestParams.type,
+                requestParams.isCategory,
+                requestParams.categoryId,
+                preference.getDBVersionByApiName("getUgcFeatureCategoryContents"),
+                request
+            )
+        }
+
+        return response.response
+    }
+
+    @AssistedInject.Factory
+    interface AssistedFactory {
+        fun create(requestParams: ApiCategoryRequestParams): GetCategoryFeatureContents
+    }
+}
