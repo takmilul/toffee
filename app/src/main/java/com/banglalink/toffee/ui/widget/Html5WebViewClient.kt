e0883caecb1901c1c04eeb61c69b546351ea2999
Shaiful <shaiful.cse@gmail.com>
2020-11-04 22:48:40 +0600

    Progress dialog now cancellable. HTML5WebView added

---
+++ app/src/main/java/com/banglalink/toffee/ui/widget/Html5WebViewClient.kt
@@ -0,0 +1,35 @@
+package com.banglalink.toffee.ui.widget
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import java.net.URISyntaxException
+
+class Html5WebViewClient:WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        if (url!!.startsWith("intent://")) {
+            try {
+                val context = view!!.context
+                val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                if (intent != null) {
+                    view.stopLoading()
+                    val packageManager: PackageManager = context.getPackageManager()
+                    val info: ResolveInfo =
+                        packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+                    if (info != null) {
+                        context.startActivity(intent)
+                    }
+                    return true
+                }
+            } catch (e: URISyntaxException) {
+                e.printStackTrace()
+            }
+        }
+
+        return false
+    }
+}
+
