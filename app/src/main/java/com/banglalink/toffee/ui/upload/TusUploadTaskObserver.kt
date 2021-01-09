064f557bdc1d77e52474c7ac39be7df3f76178bb
Shaiful <shaiful.cse@gmail.com>
2021-01-09 16:32:09 +0600

    Tus observer has been removed.

--- app/src/main/java/com/banglalink/toffee/ui/upload/TusUploadTaskObserver.kt
+++
@@ -1,7 +0,0 @@
-package com.banglalink.toffee.ui.upload
-
-import net.gotev.uploadservice.observer.task.UploadTaskObserver
-
-interface TusUploadTaskObserver: UploadTaskObserver {
-    fun onReceiveTusUploadUrl(fingerprint: String, uploadUrl: String)
-}
