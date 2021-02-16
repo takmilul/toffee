4a89ab366207cfeb4c26b2670c588c5c83bbb485
Shaiful <shaiful.cse@gmail.com>
2021-02-16 16:26:42 +0600

    UserActivities limited to 150.

--- app/src/main/java/com/banglalink/toffee/data/database/dao/UserActivitiesDao.kt
+++ app/src/main/java/com/banglalink/toffee/data/database/dao/UserActivitiesDao.kt
@@ -3,24 +3,36 @@ package com.banglalink.toffee.data.database.dao
 import androidx.paging.PagingSource
 import androidx.room.*
 import com.banglalink.toffee.data.database.entities.UserActivities
+import com.banglalink.toffee.model.USER_ACTIVITIES_LIMIT
 
 @Dao
-interface UserActivitiesDao {
+abstract class UserActivitiesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(item: UserActivities): Long
+    abstract suspend fun insertItem(item: UserActivities): Long
 
     @Update
-    suspend fun update(item: UserActivities)
+    abstract suspend fun update(item: UserActivities)
 
     @Delete
-    suspend fun delete(item: UserActivities)
+    abstract suspend fun delete(item: UserActivities)
 
     @Query("DELETE FROM UserActivities")
-    suspend fun deleteAll()
+    abstract suspend fun deleteAll()
 
     @Query("SELECT * from UserActivities WHERE customerId == :customerId ORDER BY id DESC")
-    fun getAllItems(customerId: Int): PagingSource<Int, UserActivities>
+    abstract fun getAllItems(customerId: Int): PagingSource<Int, UserActivities>
     
     @Query("DELETE FROM UserActivities WHERE customerId == :customerId AND channelId == :contentId")
-    suspend fun deleteByContentId(customerId: Int, contentId: Long)
+    abstract suspend fun deleteByContentId(customerId: Int, contentId: Long)
+
+    @Query("DELETE FROM UserActivities WHERE customerId=:customerId AND id NOT IN " +
+            "(SELECT id from UserActivities WHERE customerId=:customerId ORDER BY updateTime DESC LIMIT "+ USER_ACTIVITIES_LIMIT +")")
+    abstract suspend fun deleteExtraRows(customerId: Int)
+
+    @Transaction
+    open suspend fun insert(item: UserActivities): Long {
+        val ret = insertItem(item)
+        deleteExtraRows(item.customerId)
+        return ret
+    }
 }
