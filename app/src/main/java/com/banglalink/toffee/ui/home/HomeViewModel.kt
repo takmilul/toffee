af7453391fe82c9925674dbd98fb738dbdf94657
Shaiful <shaiful.cse@gmail.com>
2021-02-20 01:57:32 +0600

    Navigation state on Explore view all button.

--- app/src/main/java/com/banglalink/toffee/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/banglalink/toffee/ui/home/HomeViewModel.kt
@@ -66,7 +66,7 @@ class HomeViewModel @ViewModelInject constructor(
     val shareContentLiveData = SingleLiveEvent<ChannelInfo>()
     val userChannelMutableLiveData = MutableLiveData<ChannelInfo>()
     //this will be updated by fragments which are hosted in HomeActivity to communicate with HomeActivity
-    val switchBottomTab = MutableLiveData<Int>()
+    val switchBottomTab = SingleLiveEvent<Int>()
     //this will be updated by fragments which are hosted in HomeActivity to communicate with HomeActivity
     val viewAllVideoLiveData = MutableLiveData<Boolean>()
     val viewAllCategories = MutableLiveData<Boolean>()
