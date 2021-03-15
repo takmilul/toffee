a5a6e2e7a93f80922b3e5931a58e2f5e88a17d7c
Shaiful <shaiful.cse@gmail.com>
2021-03-15 18:10:20 +0600

    Enabled viewbinding for layout.

--- app/src/main/java/com/banglalink/toffee/ui/home/LandingPageFragment.kt
+++ app/src/main/java/com/banglalink/toffee/ui/home/LandingPageFragment.kt
@@ -5,10 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.activityViewModels
 import com.banglalink.toffee.R
+import com.banglalink.toffee.databinding.FragmentLandingPage2Binding
 import com.banglalink.toffee.enums.PageType.Landing
 import com.banglalink.toffee.model.ChannelInfo
 import com.banglalink.toffee.ui.common.HomeBaseFragment
@@ -16,10 +15,8 @@ import com.google.android.material.appbar.AppBarLayout
 
 class LandingPageFragment : HomeBaseFragment() {
     private var appbarOffset = 0
-    private lateinit var landingAppbar: AppBarLayout
-    private lateinit var landingCoordinator: CoordinatorLayout
-    private lateinit var latestVideoScroller: NestedScrollView
     private val landingViewModel by activityViewModels<LandingPageViewModel>()
+    private lateinit var binding: FragmentLandingPage2Binding
 
     companion object {
         fun newInstance(): LandingPageFragment {
@@ -27,15 +24,14 @@ class LandingPageFragment : HomeBaseFragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLandingPage2Binding.inflate(inflater, container, false)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (isEnabled) {
                     if (appbarOffset != 0) {
-                        latestVideoScroller.smoothScrollTo(0, 0, 0)
-                        landingAppbar.setExpanded(true, true)
+                        binding.latestVideoScroller.smoothScrollTo(0, 0, 0)
+                        binding.landingAppbar.setExpanded(true, true)
                     } else {
                         isEnabled = false
                         requireActivity().onBackPressed()
@@ -43,10 +39,7 @@ class LandingPageFragment : HomeBaseFragment() {
                 }
             }
         })
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_landing_page2, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,11 +49,7 @@ class LandingPageFragment : HomeBaseFragment() {
         landingViewModel.pageType.value = Landing
         landingViewModel.isDramaSeries.value = false
 
-        landingCoordinator = view.findViewById(R.id.landing_coordinator)
-        landingAppbar = view.findViewById(R.id.landing_appbar)
-        latestVideoScroller = view.findViewById(R.id.latestVideoScroller)
-
-        landingAppbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+        binding.landingAppbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
             appbarOffset = verticalOffset
         })
     }
