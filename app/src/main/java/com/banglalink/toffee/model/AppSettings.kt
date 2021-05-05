c4e2f8a0f277507095d83dd38d9af92e18147546
Takmilul Ehsan <takmilul.ehsan@nexdecade.com>
2021-05-05 17:09:45 +0600

    final release 2.0.0

--- app/src/main/java/com/banglalink/toffee/model/AppSettings.kt
+++ app/src/main/java/com/banglalink/toffee/model/AppSettings.kt
@@ -12,8 +12,8 @@ const val MULTI_DEVICE_LOGIN_ERROR_CODE = 109
 const val UN_ETHICAL_ACTIVITIES_ERROR_CODE = 402
 const val CLIENT_API_HEADER = "CLIENT-API-HEADER"
 
-//const val TOFFEE_BASE_URL = "https://mapi.toffeelive.com/"
-const val TOFFEE_BASE_URL = "https://staging.toffee-cms.com/"
+const val TOFFEE_BASE_URL = "https://mapi.toffeelive.com/"
+//const val TOFFEE_BASE_URL = "https://staging.toffee-cms.com/"
 //const val TOFFEE_BASE_URL = "https://ugc-staging.toffeelive.com/"
 
 val TIME_OUT_MSG = "Time out occurred. Please try later" // Need to change the message later.
