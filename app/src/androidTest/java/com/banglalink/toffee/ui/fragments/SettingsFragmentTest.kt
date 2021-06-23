f457707650c39968cce727686bd75bdce042d0ea
Shaiful <shaiful.cse@gmail.com>
2021-06-23 11:42:27 +0600

    Added Hilt specific testing setup.

--- app/src/androidTest/java/com/banglalink/toffee/ui/fragments/SettingsFragmentTest.kt
+++ app/src/androidTest/java/com/banglalink/toffee/ui/fragments/SettingsFragmentTest.kt
@@ -1,7 +1,7 @@
 package com.banglalink.toffee.ui.fragments
 
-import com.banglalink.toffee.CustomTestRunner
 import com.banglalink.toffee.data.storage.SessionPreference
+import com.banglalink.toffee.di.TestSessionPreference
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert
@@ -17,16 +17,19 @@ class SettingsFragmentTest {
     val hiltRule = HiltAndroidRule(this)
 
     @Inject
+    @TestSessionPreference
     lateinit var mPref: SessionPreference
 
     @Before
     fun setup() {
         hiltRule.inject()
+
+//        every { mPref.getDBVersionByApiName(any()) } returns 5
     }
 
     @Test
     fun getSettings() {
         println(mPref)
-//        Assert.assertEquals(false, true)
+        Assert.assertEquals(5, mPref.getDBVersionByApiName("test_db"))
     }
 }
