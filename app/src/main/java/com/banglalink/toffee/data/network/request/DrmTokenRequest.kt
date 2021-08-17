de4cd6889e26fd7506beb3a0b7f5e5a77d75e070
Takmilul Ehsan <takmilul.ehsan@nexdecade.com>
2021-08-17 13:00:55 +0600

    fixed password missing in drm token request

--- app/src/main/java/com/banglalink/toffee/data/network/request/DrmTokenRequest.kt
+++ app/src/main/java/com/banglalink/toffee/data/network/request/DrmTokenRequest.kt
@@ -3,6 +3,7 @@ package com.banglalink.toffee.data.network.request
 data class DrmTokenRequest(
     val contentID: String,
     val userID: String,
+    val password: String,
     val drmType:String = "Widevine",
     val licenseDuration: Int = 0
 ): BaseRequest("drmToken")
