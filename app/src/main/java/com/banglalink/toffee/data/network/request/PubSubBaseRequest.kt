72c12e54e038c7cac4942d724040b43a133d67d3
takmilul <takmilul.ehsan@nexdecade.com>
2022-02-09 18:13:58 +0600

    update master branch with latest code

--- app/src/main/java/com/banglalink/toffee/data/network/request/PubSubBaseRequest.kt
+++ app/src/main/java/com/banglalink/toffee/data/network/request/PubSubBaseRequest.kt
@@ -1,27 +1,29 @@
 package com.banglalink.toffee.data.network.request
 
 import android.os.Build
-import com.banglalink.toffee.BuildConfig
-import com.banglalink.toffee.data.storage.Preference
+import com.banglalink.toffee.Constants
+import com.banglalink.toffee.data.storage.CommonPreference
+import com.banglalink.toffee.data.storage.SessionPreference
 import com.banglalink.toffee.util.Utils
 import com.google.gson.annotations.SerializedName
 
 open class PubSubBaseRequest {
-    val deviceType :Int = 1
-    val appVersion : String = BuildConfig.VERSION_NAME
-    val osVersion :String = "android "+Build.VERSION.RELEASE
-    val netType:String = Preference.getInstance().netType
-    val isBlNumber = Preference.getInstance().isBanglalinkNumber
-
+    @SerializedName("deviceType")
+    val deviceType: Int = Constants.DEVICE_TYPE
+    @SerializedName("appVersion")
+    val appVersion: String = CommonPreference.getInstance().appVersionName
+    @SerializedName("osVersion")
+    val osVersion: String = "android " + Build.VERSION.RELEASE
+    @SerializedName("netType")
+    val netType: String = SessionPreference.getInstance().netType
+    @SerializedName("isBlNumber")
+    open var isBlNumber = SessionPreference.getInstance().isBanglalinkNumber
     @SerializedName("deviceId")
-    val deviceId: String = Preference.getInstance().deviceId
-
+    val deviceId: String = CommonPreference.getInstance().deviceId
     @SerializedName("customerId")
-    val customerId:Long = Preference.getInstance().customerId.toLong()
-
+    val customerId: Long = SessionPreference.getInstance().customerId.toLong()
     @SerializedName("msisdn")
-    val phoneNumber: String = Preference.getInstance().phoneNumber
-
+    open var phoneNumber: String = SessionPreference.getInstance().phoneNumber
     @SerializedName("reportingTime")
-    val reportingTime = Utils.getDateTime()
+    val reportingTime: String = Utils.getDateTime()
 }
