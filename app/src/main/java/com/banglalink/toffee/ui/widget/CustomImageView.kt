bfb1c8cfd4314a8bca24b95595e49a64fdb6d37f
Kazi Ariful Islam <kazi.ariful@nexdecade.com>
2022-02-22 12:16:33 +0600

    Changed thumbnail scaletyle into CENTER_INSIDE

--- app/src/main/java/com/banglalink/toffee/ui/widget/CustomImageView.kt
+++ app/src/main/java/com/banglalink/toffee/ui/widget/CustomImageView.kt
@@ -3,6 +3,8 @@ package com.banglalink.toffee.ui.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.banglalink.toffee.R
 
 class CustomImageView : ImageView {
     constructor(context: Context?) : super(context) {}
@@ -14,5 +16,7 @@ class CustomImageView : ImageView {
         val width = measuredWidth
         val height = width * 9 / 16
         setMeasuredDimension(width, height)
+        scaleType=ScaleType.CENTER_INSIDE
+        background=ContextCompat.getDrawable(context, R.color.fw_gnt_black)
     }
 }
