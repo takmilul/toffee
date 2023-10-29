e29ede885eebb94e30fdad376637e62127c0102d
Takmilul Ehsan <takmilul.ehsan.tonmoy@gmail.com>
2023-10-29 17:56:16 +0600

    cpp code updated

--- data/src/main/cpp/data.cpp
+++ data/src/main/cpp/data.cpp
@@ -1,12 +1,11 @@
 #include <jni.h>
 
-//std::string serverUrl = "https://mapi.toffeelive.com/";          // production server
-//std::string serverUrl = "https://j1-staging.toffeelive.com/";    // staging server
+//const char* serverUrl = "https://mapi.toffeelive.com/";          // production server
+const char* serverUrl = "https://j1-staging.toffeelive.com/";    // staging server
 
 extern "C" {
     JNIEXPORT jstring JNICALL
-    Java_com_banglalink_toffee_di_NetworkModule_getBaseUrl(JNIEnv *env, jobject thiz) {
-//        return env ->NewStringUTF("https://mapi.toffeelive.com/");
-        return env ->NewStringUTF("https://j1-staging.toffeelive.com/");
+    Java_com_banglalink_toffee_di_NetworkModule_getBaseUrl(JNIEnv *env, jobject obj) {
+        return env ->NewStringUTF(serverUrl);
     }
 }
