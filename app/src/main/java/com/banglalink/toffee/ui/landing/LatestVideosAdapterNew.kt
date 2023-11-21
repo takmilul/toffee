8891bda11adfd37ac4964f059fe69342d8b387fc
takmilul <takmilul.ehsan@nexdecade.com>
2023-11-21 18:28:21 +0600

    fixed some native ad issues

--- app/src/main/java/com/banglalink/toffee/ui/landing/LatestVideosAdapterNew.kt
+++ app/src/main/java/com/banglalink/toffee/ui/landing/LatestVideosAdapterNew.kt
@@ -11,6 +11,7 @@ import com.banglalink.toffee.R
 import com.banglalink.toffee.common.paging.BaseViewHolder
 import com.banglalink.toffee.common.paging.ItemComparator
 import com.banglalink.toffee.data.storage.SessionPreference
+import com.banglalink.toffee.databinding.ListItemVideosBinding
 import com.banglalink.toffee.model.ChannelInfo
 import com.banglalink.toffee.ui.common.ContentReactionCallback
 import kotlinx.coroutines.CoroutineScope
@@ -25,18 +26,14 @@ class LatestVideosAdapterNew(
     val cb: ContentReactionCallback<ChannelInfo>,
 ) : PagingDataAdapter<ChannelInfo, ViewHolder>(ItemComparator()) {
     
-    private var adPosition = 0
+    private val ITEM_VIEW = R.layout.list_item_videos
     private val AD_VIEW_SMALL = R.layout.item_native_ad_small
     private val AD_VIEW_LARGE = R.layout.item_native_ad_large
-    private val ITEM_VIEW = R.layout.list_item_videos
     
     override fun getItemViewType(position: Int): Int {
-        adPosition = if (position > adInterval + 1) position - 1 else position
-        if(isAdActive && position != 0 && adPosition % adInterval == 0) {
-            Log.i("View_", "AD: $position")
+        if(isAdActive && position >= adInterval && (position - ((position / adInterval)-1)) % adInterval == 0) {
             return AD_VIEW_LARGE //if (nativeAdType == SMALL) R.layout.item_native_ad_small else R.layout.item_native_ad_large
         }
-        Log.i("View_", "ITEM: $position")
         return ITEM_VIEW
     }
     
@@ -60,10 +57,12 @@ class LatestVideosAdapterNew(
                 val adViewHolder = holder as NativeAdViewHolder
                 adViewHolder.loadAds(adUnitId!!)
             }
+            Log.i("NAT_ad", "loadAds...")
             return
         }
-        val min = position / adInterval
-        val obj = getItem(position)
+        val contentPosition = if (position / adInterval > 0) (position - ((position - (position / adInterval)) / adInterval)) else position
+        Log.i("NAT_ad", "actualPos: $position, updatedPos: $contentPosition")
+        val obj = getItem(contentPosition)
         obj?.let {
             (holder as BaseViewHolder).bind(obj, cb, position)
         }
@@ -72,7 +71,7 @@ class LatestVideosAdapterNew(
     override fun getItemCount(): Int {
         val s = super.getItemCount()
         var t: Int = s + s / adInterval
-        if (s > 0) t++ // +1 when list is not empty
+//        if (s > 0) t++ // +1 when list is not empty
         return t
     }
     
@@ -80,10 +79,10 @@ class LatestVideosAdapterNew(
         return getItem(idx)
     }
     
-//    override fun onViewRecycled(holder: ViewHolder) {
-//        if (holder is BaseViewHolder && holder.binding is ListItemVideosBinding) {
-//            holder.binding.poster.setImageDrawable(null)
-//        }
-//        super.onViewRecycled(holder)
-//    }
+    override fun onViewRecycled(holder: ViewHolder) {
+        if (holder is BaseViewHolder && holder.binding is ListItemVideosBinding) {
+            holder.binding.poster.setImageDrawable(null)
+        }
+        super.onViewRecycled(holder)
+    }
 }
